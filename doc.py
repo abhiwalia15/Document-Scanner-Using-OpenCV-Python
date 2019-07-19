47837c19ed454f1d66abd115c13ef848bed4629e
mrinal <waliamrinal@gmail.com>
2019-07-19 17:52:25 +0530

    initital commit

---
+++ doc.py
@@ -0,0 +1,10 @@
+import re
+
+f = open('email.txt','r')
+contents =f.read()
+
+for o in contents.split(' '):
+    # print(o)
+    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', o)
+    if match != None:
+	    print('EMAIL:' + o)
